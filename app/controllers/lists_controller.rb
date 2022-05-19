bcdd4a8e780855403d0dee10432ed2ed8d7a072e
Bastien Grandjean <bastien.grandjean.75@gmail.com>
2022-05-19 17:30:34 +0200

    website squelette et un peu de CSS

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,35 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @bookmarks = @list.bookmarks
+  end
+
+  def create
+    @lists = List.all
+    @list = List.new(list_params)
+    @list.save
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render 'index', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to :lists, status: :see_other
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: %i[index show create destroy] do
+    resources :bookmarks, only: %i[create]
+  end
+  resources :bookmarks, only: %i[destroy]
 end
